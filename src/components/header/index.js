f6408256856eb4a6206ce5704686856ae202a98a
Bruno Paulino <marinho.bruno@live.com>
2022-07-07 14:40:30 -0300

    Subtittle removed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,7 +12,6 @@ export default function Header({ onDataUser }) {
     <S.DivHead>
       <S.Tittle>Welcome to</S.Tittle>
       <S.Logo>GIFSEARCH</S.Logo>
-      <S.Subtittle>Because a gif is worth a thousand words!</S.Subtittle>
       <form onSubmit={sendData}>
         <S.Input
           autoFocus
